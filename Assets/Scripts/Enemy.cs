e72a1082fbe0212279c37adaf1aac11fea8608be
Noyan Berkay Aşkın <berkay4.askin@hotmail.com>
2022-07-30 16:36:58 +0300

    Enemy has knockback function now
    When an enemy get hit it is also knockbacked backwards to player.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -10,24 +10,34 @@ public class Enemy : MonoBehaviour
     public ParticleSystem particle;
     public GameObject enemySpawnerList;
     public EquipmentItem magicMissleItem;
+    private Rigidbody2D enemyRb;
+    private bool isKnockBacked = false;
+    
 
     private void Start()
     {
         player = GameObject.Find("Player");
         enemySpawnerList = GameObject.Find("EnemySpawner");
+        enemyRb = gameObject.GetComponent<Rigidbody2D>();
+        
+        
     }
+
+    
 
     private void Update()
     {
         if(player != null)
+        
         {
+             //enemyRb.velocity = (player.transform.position - transform.position) * speed*Time.deltaTime;
             transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
             
         }
         
     }
     
-    public void GetDamaged(float damage)
+    public void GetDamaged(float damage, float knockBackPower, float knockBackDelay)
     {
         
         health -= damage;
@@ -40,6 +50,23 @@ public class Enemy : MonoBehaviour
             Destroy(gameObject);
 
         }
+        if(knockBackPower > 0 && knockBackDelay > 0)
+        {
+            StartCoroutine(EnemyKnockBack(knockBackPower,knockBackDelay));
+        }
+        if(isKnockBacked == true)
+        {
+            StopCoroutine(EnemyKnockBack(knockBackPower,knockBackDelay));
+            isKnockBacked = false;
+        }
+    }
+
+    IEnumerator EnemyKnockBack(float knockbackPower,float knockbackDelay)
+    {
+        enemyRb.AddForce((transform.position - player.transform.position) * knockbackPower,ForceMode2D.Impulse);
+        yield return new WaitForSeconds(knockbackDelay);
+        enemyRb.velocity = Vector2.zero;
+        isKnockBacked = true;
     }
 
 }
